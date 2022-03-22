48201cb677f5e7ccbb1bebb679b756e94a1e7338
Mahamadou <camaram9@lsbu.ac.uk>
2022-03-22 06:15:39 +0000

    Change class EnvoyerMail.php

--- classes/EnvoyerMail.php
+++ classes/EnvoyerMail.php
@@ -1,13 +1,76 @@
 <?php
-include('../../cnx/cnx.php');
-spl_autoload_register(function($class){
-    include('../../classes/'.$class.'.php');
-});
+class EnvoyerMail extends Commande {
 
-$cdeM = new CommandeManager($cnx);
-$cde  = $cdeM->ReadCde($_POST['reference']);
+    public function __construct($prenom, $nom, $email, $ref) {
+        $this->setPrenom($prenom);
+        $this->setNom($nom);
+        $this->setEmail($email);
+        $this->setRef($ref);
 
-$cdeM->UpdateSwitch($_POST['c'], $_POST['reference']);
+    }
+    
+    public function EnvoiMail() {
+        $expediteur   = 'no-reply@kono-dev.com';
+        $ref          = $this->getRef();
+        $prenom       = $this->getPrenom();
+        $nom          = $this->getNom();
+        $destinataire = $this->getEmail();
 
-$envoyer = new EnvoyerMail($cde->getPrenom(), $cde->getNom(), $cde->getEmail(), $cde->getRef());
-$envoyer->EnvoiMail();
+        $sujet        = 'New event concerning your order ref.' .$ref;
+        $contenu      = '
+                <!DOCTYPE html>
+                <html lang="en">
+                
+                <head>
+                    <meta charset="UTF-8">
+                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>New event concerning your order</title>
+                    <style>
+                        #box {
+                            width: 75%;
+                            margin: auto;
+                            font-family: sans-serif;
+                        }
+                
+                        header {
+                            background-color: rgb(72, 8, 247);
+                            color: aliceblue;
+                            text-align: center;
+                            padding: 10px 10px;
+                        }
+                
+                        a {
+                            color: blue;
+                        }
+                    </style>
+                </head>
+                
+                <body>
+                    <div id="box">
+                        <header>
+                            <h2>MyWebSite.com</h2>
+                            <h3>New event concerning your order réf. '.$ref.'</h3>
+                        </header>
+                        <article>
+                            <h4>Hello '.$prenom.' '.$nom.',</h4>
+                            <P>
+                                A new event concerning your order
+                                ref. '.$ref.' has just been declared.
+                            </P>
+                            <p><a href="https://www.kono-dev.com/projets/trackingparcelapp/suivi/index.php?ref='.$ref.'">Cliquer ici pour voir ce nouvel evenement</a></p>
+                            <p>
+                                <em>We thank you for your confidence,<br>MyWebSite.com</em>
+                            </p>
+                        </article>
+                    </div>
+                </body>
+                
+                </html>
+
+        ';
+        $header       = "FROM:$expediteur \n";
+        $header       .= "Content-Type:text/html; charset='utf-8'";
+        mail($destinataire, $sujet, $contenu, $header);
+    }
+}
